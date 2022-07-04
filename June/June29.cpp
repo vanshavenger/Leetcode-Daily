de8c377c44379799b81e32332755816f3d4b690e
vanshavenger <76000026+vanshavenger@users.noreply.github.com>
2022-07-04 13:37:24 +0530

    Create June29.cpp

---
+++ June/June29.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(),people.end(),[&](vector<int>&a,vector<int>&b){
+           if(a[0] == b[0]){
+               return a[1] < b[1];
+           } 
+            return a[0] > b[0];
+        });
+        vector<vector<int>> ans;
+        for(auto &it:people){
+            ans.insert(ans.begin() + it[1],it);
+        }
+        return ans;
+    }
+};
