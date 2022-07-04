d1f803bf3d9c3e4f474dbc8135994626b979a800
vanshavenger <76000026+vanshavenger@users.noreply.github.com>
2022-07-04 13:24:50 +0530

    Create June13.cpp

---
+++ June/June13.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int solve(int i,int j,int n,vector<vector<int>>& triangle,vector<vector<int>>& dp){
+        if(i == n-1){
+            return triangle[i][j];
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        return dp[i][j] = min(solve(i+1,j,n,triangle,dp),solve(i+1,j+1,n,triangle,dp)) + triangle[i][j];
+    }
+    
+    
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return solve(0,0,n,triangle,dp);
+        
+    }
+};
