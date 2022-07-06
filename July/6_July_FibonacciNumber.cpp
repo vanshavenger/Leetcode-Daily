0e7a79ce7244f4a57d059ade9bb2584a6781f209
vanshavenger <76000026+vanshavenger@users.noreply.github.com>
2022-07-06 14:49:58 +0530

    Create 6_July_FibonacciNumber.cpp

---
+++ July/6_July_FibonacciNumber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0 || n == 1){
+            return n;
+        }
+        int* ans = new int[n+1];
+        ans[0] = 0;
+        ans[1] = 1;
+        
+        for(int i = 2;i<=n;i++){
+            ans[i] = ans[i-1]+ans[i-2];
+        }
+        int a = ans[n];
+        delete [] ans;
+        return a;
+    }
+};
